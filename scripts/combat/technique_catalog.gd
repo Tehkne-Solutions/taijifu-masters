1e0db4fe1bde93cf6998a1f16d11b23de23f40c3
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-26 13:29:18 -0300

    feat: add grab technique and disarm pressure

--- scripts/combat/technique_catalog.gd
+++ scripts/combat/technique_catalog.gd
@@ -20,6 +20,7 @@ static func get_technique(technique_id: StringName) -> TechniqueData:
 			technique.hitbox_size = Vector2(72, 34)
 			technique.hitbox_offset = Vector2(47, -21)
 			technique.grounded_only = true
+			technique.disarm_pressure = 5.0
 		&"tai_aerial_arc":
 			technique.display_name = "Arco Ascendente"
 			technique.path = "tai"
@@ -48,6 +49,7 @@ static func get_technique(technique_id: StringName) -> TechniqueData:
 			technique.hitbox_size = Vector2(48, 40)
 			technique.hitbox_offset = Vector2(28, -22)
 			technique.grounded_only = true
+			technique.disarm_pressure = 8.0
 		&"ji_sweep":
 			technique.display_name = "Varredura de Base"
 			technique.path = "ji"
@@ -62,6 +64,7 @@ static func get_technique(technique_id: StringName) -> TechniqueData:
 			technique.hitbox_size = Vector2(68, 24)
 			technique.hitbox_offset = Vector2(40, 4)
 			technique.grounded_only = true
+			technique.disarm_pressure = 7.0
 		&"ji_shove":
 			technique.display_name = "Empurrão de Fundação"
 			technique.path = "ji"
@@ -77,6 +80,21 @@ static func get_technique(technique_id: StringName) -> TechniqueData:
 			technique.hitbox_offset = Vector2(32, -23)
 			technique.grounded_only = true
 			technique.causes_disarm_pressure = true
+			technique.disarm_pressure = 24.0
+		&"ji_clinch_grab":
+			technique.display_name = "Laço de Centro"
+			technique.path = "ji"
+			technique.interaction_type = "grab"
+			technique.startup_frames = 7
+			technique.active_frames = 5
+			technique.recovery_frames = 18
+			technique.grab_hold_seconds = 0.46
+			technique.stamina_cost = 14.0
+			technique.damage = 0.0
+			technique.posture_damage = 0.0
+			technique.hitbox_size = Vector2(48, 54)
+			technique.hitbox_offset = Vector2(27, -22)
+			technique.grounded_only = true
 		&"fu_flow_strike":
 			technique.display_name = "Golpe de Transição"
 			technique.path = "fu"
@@ -91,6 +109,7 @@ static func get_technique(technique_id: StringName) -> TechniqueData:
 			technique.hitbox_size = Vector2(58, 42)
 			technique.hitbox_offset = Vector2(36, -24)
 			technique.can_turn_during_startup = true
+			technique.disarm_pressure = 5.0
 		&"fu_reversal":
 			technique.display_name = "Reversão do Fluxo"
 			technique.path = "fu"
@@ -104,6 +123,7 @@ static func get_technique(technique_id: StringName) -> TechniqueData:
 			technique.vertical_force = -150.0
 			technique.hitbox_size = Vector2(64, 48)
 			technique.hitbox_offset = Vector2(16, -26)
+			technique.disarm_pressure = 11.0
 		_:
 			technique.display_name = "Técnica não catalogada"
 
