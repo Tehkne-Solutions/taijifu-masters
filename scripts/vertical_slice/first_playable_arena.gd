414cac1d03d9e0af8ff2f4755a1f5fd93beeb5c4
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-30 19:39:43 -0300

    feat: synchronize First Playable environment state

--- scripts/vertical_slice/first_playable_arena.gd
+++ scripts/vertical_slice/first_playable_arena.gd
@@ -1,8 +1,18 @@
 class_name FirstPlayableArena
 extends TriplePathArena
 
+func start_battle_flow() -> void:
+	super.start_battle_flow()
+	_set_environment_round_active(true)
+
 func stop_battle_flow() -> void:
 	_battle_active = false
 	_active_manifestation = -1
 	_manifestation_timer = 0.0
+	_set_environment_round_active(false)
 	queue_redraw()
+
+func _set_environment_round_active(active: bool) -> void:
+	var environment := get_node_or_null("../EnvironmentArt") as FirstPlayableEnvironmentArt
+	if is_instance_valid(environment):
+		environment.set_round_active(active)
