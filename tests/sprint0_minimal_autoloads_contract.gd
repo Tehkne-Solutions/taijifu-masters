36da198b13a8bc6421fc2783e06cfeb0bb188963
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-31 21:15:14 -0300

    test: fixar núcleo mínimo de autoloads

---
+++ tests/sprint0_minimal_autoloads_contract.gd
@@ -0,0 +1,65 @@
+extends SceneTree
+
+const MAX_AUTOLOADS := 12
+const REQUIRED := [
+	"TgapAssetLoader",
+	"AssetPackRegistry",
+	"Pack99BattleVisualRuntime",
+	"Pack99CombatEventRuntime",
+	"Pack08ArenaUnitRuntime",
+	"GameModeRuntime",
+	"TaijifuWebBridge",
+]
+const FORBIDDEN := [
+	"SeriesLootProgressionRuntime",
+	"CompleteSeriesModeRuntime",
+	"ModeAwarePreparationRuntime",
+	"CombatDifficultyTrainingRuntime",
+	"TrainingProgressionRuntime",
+	"PlayerProgressionProfileRuntime",
+	"CosmeticCollectionRuntime",
+	"PurchasedItemsApplicationRuntime",
+	"PreparationBuildComparisonRuntime",
+	"TaijifuGamepadTraining",
+	"TaijifuGamepadExperience",
+	"TaijifuControllerMastery",
+	"TaijifuInputGhostMastery",
+	"TaijifuGhostSharing",
+	"TaijifuGhostLibrary",
+	"TaijifuGhostRaceHistory",
+	"TaijifuGhostRaceSeries",
+	"TaijifuGhostRivalRanking",
+	"TaijifuMultiGhostRace",
+	"TaijifuGhostRace",
+]
+
+func _init() -> void:
+	var file := FileAccess.open("res://project.godot", FileAccess.READ)
+	if file == null:
+		_fail("project.godot não pôde ser lido")
+		return
+	var source := file.get_as_text()
+	var section := source.get_slice("[autoload]", 1).get_slice("[debug]", 0)
+	var count := 0
+	for line in section.split("\n"):
+		if line.contains("=\"*res://"):
+			count += 1
+	if count > MAX_AUTOLOADS:
+		_fail("autoloads acima do limite: %d > %d" % [count, MAX_AUTOLOADS])
+		return
+	for name in REQUIRED:
+		if not section.contains(name + "="):
+			_fail("autoload obrigatório ausente: " + name)
+			return
+	for name in FORBIDDEN:
+		if section.contains(name + "="):
+			_fail("autoload fora do First Playable ainda ativo: " + name)
+			return
+	print("SPRINT0_MINIMAL_AUTOLOADS_CONTRACT_OK")
+	quit(0)
+
+func _fail(message: String) -> void:
+	push_error(message)
+	quit(1)
+
+# Tehkné Solutions
