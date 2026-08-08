3c8fa91c698aa7b7cffc0a355073f91c8a91c92a
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-08-08 16:52:23 -0300

    fix(c62.3): make creator signal smoke deterministic

--- scripts/ci/base01_skin_creator_control_smoke.gd
+++ scripts/ci/base01_skin_creator_control_smoke.gd
@@ -13,8 +13,15 @@ const EXPECTED_IDS := [
 ]
 const DEFAULT_ID := "skin_tone_03_warm"
 
+var _signal_count := 0
+var _last_signal: StringName = &""
+
 func _init() -> void:
 	call_deferred("_run")
+
+func _on_skin_selected(palette_id: StringName) -> void:
+	_signal_count += 1
+	_last_signal = palette_id
 
 func _run() -> void:
 	var failures := PackedStringArray()
@@ -50,12 +57,7 @@ func _run() -> void:
 	if Array(ids) != EXPECTED_IDS:
 		failures.append("palette_ids:%s" % JSON.stringify(Array(ids)))
 
-	var signal_count := 0
-	var last_signal := &""
-	selector.skin_selected.connect(func(palette_id: StringName):
-		signal_count += 1
-		last_signal = palette_id
-	)
+	selector.skin_selected.connect(_on_skin_selected)
 
 	var bind_failures := selector.bind(profile, assembler)
 	if not bind_failures.is_empty():
@@ -95,10 +97,10 @@ func _run() -> void:
 			failures.append("display_name_empty:%s" % palette_id)
 		print("C62_3_CREATOR_OPTION=PASS palette=%s" % palette_id)
 
-	if signal_count != expected_signals:
-		failures.append("signal_count:%d expected:%d" % [signal_count, expected_signals])
-	if last_signal != StringName(EXPECTED_IDS[-1]):
-		failures.append("last_signal:%s" % String(last_signal))
+	if _signal_count != expected_signals:
+		failures.append("signal_count:%d expected:%d" % [_signal_count, expected_signals])
+	if _last_signal != StringName(EXPECTED_IDS[-1]):
+		failures.append("last_signal:%s" % String(_last_signal))
 
 	var before_invalid := selector.selected_palette_id()
 	var invalid_failures := selector.select_palette(&"skin_tone_99_invalid")
